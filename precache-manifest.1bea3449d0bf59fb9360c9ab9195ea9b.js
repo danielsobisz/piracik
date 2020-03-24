8fee107bc1ce6bbfdfc685043cffa31929446326
Daniel Sobisz <dsobisz97@gmail.com>
2020-03-24 19:25:30 +0100

    Updates

---
+++ precache-manifest.1bea3449d0bf59fb9360c9ab9195ea9b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77897ab007c488e5f9cdb677c8b0e694",
+    "url": "/piracik/index.html"
+  },
+  {
+    "revision": "95d96532329dfaf264b8",
+    "url": "/piracik/static/css/main.e49d9de8.chunk.css"
+  },
+  {
+    "revision": "7e970a83f4f0f0ae799d",
+    "url": "/piracik/static/js/2.aac6602e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/piracik/static/js/2.aac6602e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95d96532329dfaf264b8",
+    "url": "/piracik/static/js/main.952a384a.chunk.js"
+  },
+  {
+    "revision": "3743c913681964537213",
+    "url": "/piracik/static/js/runtime-main.a46c2a3d.js"
+  },
+  {
+    "revision": "5538246baa8b06c7d83e232e801b6dc0",
+    "url": "/piracik/static/media/apartment.5538246b.jpg"
+  },
+  {
+    "revision": "dc889fed5dfee0d83afa89d5f4354c60",
+    "url": "/piracik/static/media/background-landscape.dc889fed.jpg"
+  },
+  {
+    "revision": "f5c6c962396f9ce09d7b85513f94a9e9",
+    "url": "/piracik/static/media/background-mobile.f5c6c962.jpg"
+  },
+  {
+    "revision": "e43f6e9d63a2c695f75a745b7a4480e4",
+    "url": "/piracik/static/media/background.e43f6e9d.jpg"
+  },
+  {
+    "revision": "d4fd4defe9904d26d5868b969c131b5c",
+    "url": "/piracik/static/media/backgrounddiv.d4fd4def.jpg"
+  },
+  {
+    "revision": "d307178551d6b484500ab8788d803621",
+    "url": "/piracik/static/media/delux.d3071785.jpg"
+  },
+  {
+    "revision": "5dfd60f90f0a82d77654b2e04a416ef5",
+    "url": "/piracik/static/media/header-landscape.5dfd60f9.jpg"
+  },
+  {
+    "revision": "a5a99be28c9b73723f227c234651a785",
+    "url": "/piracik/static/media/header-mobile.a5a99be2.jpg"
+  },
+  {
+    "revision": "90a72bca02d79d6a55f4c0e18870dc77",
+    "url": "/piracik/static/media/header-tablet.90a72bca.JPG"
+  },
+  {
+    "revision": "e091daf5c69ebe047cc954cdd5e824ff",
+    "url": "/piracik/static/media/header.e091daf5.jpg"
+  },
+  {
+    "revision": "fc5b0923f7ad286d3ae7e7aec149b97c",
+    "url": "/piracik/static/media/standard.fc5b0923.jpg"
+  }
+]);
